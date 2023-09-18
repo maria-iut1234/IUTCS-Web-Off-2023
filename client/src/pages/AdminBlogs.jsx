9384e461d47ec45c94e8b9ad09557870f5d9cd69
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-19 03:06:02 +0600

    blog er bhanga version xD

---
+++ client/src/pages/AdminBlogs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminNavbar from "../components/AdminNavbar";
+import BlogFeed from "../components/BlogFeed";
+
+const AdminBlogs = () => {
+  return (
+    <div className="flex flex-col min-h-screen gradient-bg bg-cover bg-fixed bg-center">
+        <AdminNavbar/>
+        <BlogFeed/>
+    </div>
+  );
+};
+
+export default AdminBlogs;
