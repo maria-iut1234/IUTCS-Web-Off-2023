5050f599d2cacfbbcbb2e7e232e8200a1e7e1cd9
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-23 02:29:35 +0600

    restructured files

--- client/src/pages/ExecutivesInfoPage.jsx
+++ client/src/pages/ExecutivesInfoPage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import AdminNavbar from '../components/AdminNavbar'
 
 const ExecutivesInfoPage = () => {
+  const admin = true; //CHECK FOR ADMIN
+  const bgClass = admin? "admin-gradient-bg" : "user-gradient-bg"
   return (
-    <div className="flex flex-col min-h-screen admin-gradient-bg bg-cover bg-fixed bg-center">
+    <div className={`flex flex-col min-h-screen ${bgClass} bg-cover bg-fixed bg-center`}>
       <AdminNavbar />
     </div>
   )
