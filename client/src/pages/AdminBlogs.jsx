8207a12bb046e850060ae120604eed43cdbe3c36
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-22 22:58:56 +0600

    changed css class names

--- client/src/pages/AdminBlogs.jsx
+++ client/src/pages/AdminBlogs.jsx
@@ -4,7 +4,7 @@ import BlogFeed from "../components/BlogFeed";
 
 const AdminBlogs = () => {
   return (
-    <div className="flex flex-col min-h-screen gradient-bg bg-cover bg-fixed bg-center">
+    <div className="flex flex-col min-h-screen admin-gradient-bg bg-cover bg-fixed bg-center">
         <AdminNavbar/>
         <BlogFeed/>
     </div>
