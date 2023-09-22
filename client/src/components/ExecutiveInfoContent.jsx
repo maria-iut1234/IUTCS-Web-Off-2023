1342823737608f127e087c9123416fbb078b103b
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-22 23:10:39 +0600

    added executives info template

---
+++ client/src/components/ExecutiveInfoContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExecutiveInfoContent = () => {
+  return (
+    <div>ExecutiveInfoContent</div>
+  )
+}
+
+export default ExecutiveInfoContent
---
+++ client/src/pages/AdminExecutivesInfoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminNavbar from '../components/AdminNavbar'
+
+const ExecutivesInfoPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen admin-gradient-bg bg-cover bg-fixed bg-center">
+      <AdminNavbar />
+    </div>
+  )
+}
+
+export default ExecutivesInfoPage
