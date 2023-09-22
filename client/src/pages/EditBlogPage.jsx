e248d2176e6332d9b332b079737f9fe6a6124977
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-23 02:30:18 +0600

    incominggg edit blog feature

---
+++ client/src/pages/EditBlogPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminNavbar from "../components/AdminNavbar";
+
+const EditBlogPage = () => {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen admin-gradient-bg bg-cover bg-fixed bg-center">
+      </div>
+    </>
+  );
+};
+
+export default EditBlogPage;
