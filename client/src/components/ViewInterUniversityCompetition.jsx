91630cd0bb4cd0181d43ebac53e09944abee204c
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-24 02:00:33 +0600

    view competitions page basic done
    -info table changes with the structure of data (somewhat...)

--- client/src/components/ViewInterUniversityCompetition.jsx
+++ client/src/components/ViewInterUniversityCompetition.jsx
@@ -12,18 +12,21 @@ const ViewInterUniversityCompetition = () => {
       name: "Competition Type 1",
       description: "Description for Competition Type 1",
       date: "2023-09-16T00:00:00.000Z",
+      competition_type: "team", // Added competition_type
     },
     {
       competition_id: "2",
       name: "Competition Type 2",
       description: "Description for Competition Type 2",
       date: "2023-09-17T00:00:00.000Z",
+      competition_type: "individual", // Added competition_type
     },
     {
       competition_id: "3",
       name: "Competition Type 3",
       description: "Description for Competition Type 3",
       date: "2023-09-18T00:00:00.000Z",
+      competition_type: "team", // Added competition_type
     },
   ];
 
@@ -33,36 +36,41 @@ const ViewInterUniversityCompetition = () => {
       competition_id: "1",
       participant_name: "Participant 1",
       competition_name: "Competition Type 1",
+      team_name: "Team A",
+      team_size: 5,
+      date_of_registration: "2023-09-15",
     },
     {
       _id: 2,
       competition_id: "1",
       participant_name: "Participant 2",
       competition_name: "Competition Type 1",
+      team_name: "Team B",
+      team_size: 3,
+      date_of_registration: "2023-09-16",
     },
     {
       _id: 3,
       competition_id: "2",
       participant_name: "Participant 3",
       competition_name: "Competition Type 2",
+      date_of_registration: "2023-09-17",
     },
     {
       _id: 4,
       competition_id: "3",
       participant_name: "Participant 4",
       competition_name: "Competition Type 3",
+      team_name: "Team C",
+      team_size: 4,
+      date_of_registration: "2023-09-18",
     },
     {
       _id: 5,
       competition_id: "2",
       participant_name: "Participant 5",
       competition_name: "Competition Type 2",
-    },
-    {
-      _id: 6,
-      competition_id: "3",
-      participant_name: "Participant 6",
-      competition_name: "Competition Type 3",
+      date_of_registration: "2023-09-19",
     },
   ];
 
@@ -94,7 +102,10 @@ const ViewInterUniversityCompetition = () => {
       </div>
 
       {selectedCompetition && (
-        <Table data={filteredParticipantsData} /> // Wrap the selectedCompetition in an array
+        <Table
+          data={filteredParticipantsData}
+          selectedCompetition={selectedCompetition}
+        />
       )}
     </div>
   );
