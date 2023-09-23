c6aa1f90b2eae9a66af04ddd7cc4404b3827a9f8
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-24 02:01:27 +0600

    added sign out button in admin navbar

--- client/src/components/AdminNavbar.jsx
+++ client/src/components/AdminNavbar.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/IUTCS_logo_transparent_light.png";
+import { FiLogOut } from "react-icons/fi";
 
 const AdminNavbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [cancelMouseLeave, setCancelMouseLeave] = useState(false); // Step 1
 
   const linkClass =
-    "py-4 md:mx-8 md:py-0 relative text-lg w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#02E1DC] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center hover:text-[#02E1DC]";
+    "py-4 lg:mx-8 lg:py-0 relative text-lg w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#02E1DC] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center hover:text-[#02E1DC]";
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -30,9 +30,9 @@ const AdminNavbar = () => {
         </div>
       </div>
       <nav className="flex justify-between bg-black text-white w-full">
-        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+        <div className="px-5 xl:px-12 py-4 flex w-full items-center">
           <ul
-            className={`md:flex px-4 mx-auto font-semibold font-heading ${
+            className={`lg:flex px-4 mx-auto font-semibold font-heading ${
               isMenuOpen ? "block" : "hidden"
             }`}
           >
@@ -66,13 +66,30 @@ const AdminNavbar = () => {
               </Link>
             </li>
 
-            
+            <li>
+              <Link to="/home" className={`text-[#02e1dc] ${linkClass}`}>
+                <div>
+                  <div className="z-10" onClick={() => {}}>
+                    <button
+                      type="submit"
+                      className="relative -top-1 -left-1 bg-[#F4F2F0] py-1 px-2 font-medium text-md md:text-lg text-[#373234] transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-[#02E1DC] border-[#02E1DC] before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0 flex items-center"
+                    >
+                      <div className="inline-block mr-2">
+                        <FiLogOut />
+                      </div>
+                      Sign Out
+                    </button>
+                  </div>
+                </div>
+              </Link>
+            </li>
           </ul>
         </div>
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button
             type="button"
-            className="pt-1 w-10 h-10 text-sm text-white hover:text-[#02E1DC]"
+            className=" w-10 h-10 text-sm text-white hover:text-[#02E1DC]"
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg
