10cbbc3a87f4186eeccd262f6bbc610dc98802fb
Nazmul Hossain <nazmulhossain@iut-dhaka.edu>
2023-09-23 16:16:20 +0600

    Added About Us Page

--- client/src/components/AboutUsContent.jsx
+++ client/src/components/AboutUsContent.jsx
@@ -1,9 +1,117 @@
 import React from 'react'
 
+
 const AboutUsContent = () => {
   return (
     <>
-    {/* content here~ */}
+      <div
+        className="mx-auto px-4 sm:px-6 pt-12 pt-6"
+      >
+        <div class="flex flex-wrap" >
+        <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+              <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                  <i class="fa-solid fa-list"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Blogs</h6>
+                <p class="mt-2 mb-4 text-white">
+                  Read blogs published by the executives at IUTCS.
+                  Learn about all the new events and happenings.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words  mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
+                  <i class="fa-solid fa-terminal"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Events</h6>
+                <p class="mt-2 mb-4 text-white">
+                  Register for any upcoming exciting Event organized by IUTCS.
+                  Remember, you lose every shot you don't take.
+                </p>
+              </div>
+              
+            </div>
+          </div>
+          <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-yellow-400">
+                <i class="fa-solid fa-code"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Activities</h6>
+                <p class="mt-2 mb-4 text-white">
+                  Attend our ACM, WebDev, AppDev, Research Seminars for experiences that can benefit your career and personal development.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+              <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                  <i class="fas fa-award"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Achievements</h6>
+                <p class="mt-2 mb-4 text-white">
+                View all the latest achievements of the IUTCS family.
+                  We are proud of our members and their achievements.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words  mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-purple-400">
+                  <i class="fas fa-retweet"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Vision</h6>
+                <p class="mt-2 mb-4 text-white">
+                  Allow students to become more involved in the field of computer science and make them more proficient in their respective fields.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+            <div class="relative flex flex-col min-w-0 break-words mb-8 shadow-lg rounded-lg bg-blue-200 bg-opacity-30 p-6 drop-shadow-xl " style={{
+              backdropFilter: "blur(15px)",
+              boxShadow: "0 0 60px rgba(2, 225, 220, 0.4)",
+            }}>
+              <div class="px-4 py-5 flex-auto">
+                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-orange-400">
+                  <i class="fas fa-fingerprint"></i>
+                </div>
+                <h6 class="text-xl text-red-500 font-semibold">Research</h6>
+                <p class="mt-2 mb-4 text-white">
+                Members engage in groundbreaking research, contributing to advancements in AI, cybersecurity, data science, and more.
+                </p>
+              </div>
+            </div>
+          </div>
+          
+          
+        </div>
+      </div>
     </>
   )
 }
