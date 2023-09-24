f896b0bfb7d969c69d8209710c8935033b035593
Shanta Maria <shantamaria@iut-dhaka.edu>
2023-09-24 23:52:05 +0600

    activities section with animated card

---
+++ client/src/components/ActivitiesSection.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import LandingPageCard from "./LandingPageCard";
+
+function ActivitiesSection() {
+  return (
+    <>
+      <div
+        className={`flex-col bg-white bg-cover bg-fixed bg-center justify-center items-center`}
+      >
+        <h1 className="font-poppins-custom text-black text-7xl transition-colors duration-300 hover:text-red-500">
+          Activites
+        </h1>
+        <div
+          className={`flex flex-wrap bg-white bg-cover bg-fixed bg-center mx-auto justify-center items-center`}
+        >
+          <LandingPageCard
+            className="px-7"
+            coverImgUrl={
+              "https://img.freepik.com/free-vector/programming-languages-learning-software-coding-courses-website-development-class-script-writing-it-programmers-cartoon-characters_335657-3454.jpg?w=740&t=st=1695572007~exp=1695572607~hmac=e577448a88bf273f5a55dbd4818a89cac1840e6a9387431e00444dfd3cf97bd9"
+            }
+            hoverImgUrl={
+              "https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/367987827_678385974328540_2086429872797664196_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeG41rLWE-E-uiqWLoWm64xc7OLl4zwaJ9bs4uXjPBon1nYE0gR09sqJ_rvaNJ_fclfmVZ2CnqWj0LFCM-9laTnV&_nc_ohc=6UowYzmWYOsAX8gcutC&_nc_ht=scontent-sin6-2.xx&oh=00_AfCDo0MXVpKSytbzVxrYq5rkXswJ2POMLO8_LsdWBclIKg&oe=6515FD70"
+            }
+            header={"Competitive Programming"}
+          />
+
+          <LandingPageCard
+            coverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            hoverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            header={"Capture The Flag (CTF)"}
+          />
+          <LandingPageCard
+            coverImgUrl={
+              "https://img.freepik.com/free-vector/programming-concept-illustration_114360-1351.jpg?w=740&t=st=1695571590~exp=1695572190~hmac=a539959b050342437b9603e343d73286cf23e2b46649268ff92c8616062babc2"
+            }
+            hoverImgUrl={
+              "https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/377950433_694666892700448_4051838056346933253_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeEoEMtBq0eJFInT3q2rxsxaTqDgkLvjj_lOoOCQu-OP-VjW4miPxQzYYiVVg9unSioQ36iYjfHzccfkJIDrZz-_&_nc_ohc=pwMtMEDMX8MAX_MmByc&_nc_ht=scontent-sin6-1.xx&oh=00_AfDfR4aKbVRFJMgKOPs1wb4xRnbdqWiPI1Jvul0n_O0Tdg&oe=6515B5CE"
+            }
+            header={"Hackathon"}
+          />
+          <LandingPageCard
+            coverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            hoverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            header={"IT Business Case"}
+          />
+          <LandingPageCard
+            coverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            hoverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            header={"Olympiad"}
+          />
+          <LandingPageCard
+            coverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            hoverImgUrl={
+              "https://img.freepik.com/free-photo/3d-internet-secuirty-badge_1048-18107.jpg?w=740&t=st=1695568224~exp=1695568824~hmac=32cdfca3675bb746a879e4814aefe8247837c3d8697d582320a3a9c74714007b"
+            }
+            header={"E-Sports"}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ActivitiesSection;
---
+++ client/src/components/LandingPageCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function LandingPageCard({ coverImgUrl, hoverImgUrl, header }) {
+  return (
+    <div className="w-1/3 pl-24 py-7 mx-auto">
+      <div className="group overflow-hidden relative shadow-lg max-w-xs">
+        <a href="#" className="absolute z-10 top-0 bottom-0 left-0 right-0"></a>
+        <img
+          className="block group-hover:opacity-40 transition-opacity duration-700"
+          src={coverImgUrl}
+          alt="Game Cover"
+        />
+        <div className="absolute bg-black flex items-center group-hover:-top-0 group-hover:opacity-80 duration-700 top-full right-0 w-full opacity-0 h-1/2 transition-all">
+          <div
+            className=""
+            style={{
+              backgroundImage: `url(${hoverImgUrl})`,
+            }}
+          >
+            <img className="w-full" src={hoverImgUrl} alt="Hover Image" />
+          </div>
+        </div>
+        <div className="absolute bg-gradient-to-br duration-500 from-[#002766] to-[#00002b] text-white block left-0 right-0 top-full text-base h-1/2 w-full opacity-50 transition-all group-hover:top-1/2 group-hover:opacity-100">
+          <div className="py-4 text-xs px-7">
+            <div className="text-3xl font-bold text-[#FCCD0D] pb-4">{header}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPageCard;
