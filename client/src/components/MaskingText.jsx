9a56047895c2a0d8e6b095033c6587e92bca2f70
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-09-29 22:09:04 +0600

    tried to fix some css in landing page

--- client/src/components/MaskingText.jsx
+++ client/src/components/MaskingText.jsx
@@ -57,19 +57,19 @@ const MaskingText = () => {
             className={"w-full"}
           />
         </div>
-        <div className="flex justify-center items-center h-screen hide-cursor">
+        <div className="flex justify-center items-center md:h-screen hide-cursor">
           <div>
             <h1
               onMouseEnter={textEnter}
               onMouseLeave={textLeave}
-              className="font-digital7-custom px-10 md:px-20 py-10 text-white text-9xl transition-colors duration-300 hover:text-[#FCCD0D]"
+              className="font-digital7-custom px-0 lg:px-0 md:px-10 py-10 text-white text-[6em] sm:text-[8em] md:text-[5em] lg:text-[8em] transition-colors duration-300 hover:text-[#FCCD0D]"
             >
               {`I U T C S`}
             </h1>
             <div
               onMouseEnter={textEnter}
               onMouseLeave={textLeave}
-              className="max-w-screen md:ml-24 ml-10 mb-6 text-4xl lg:text-[36px] leading-[1] md:leading-[0.5]"
+              className="max-w-screen md:ml-10 lg:ml-0 ml-0 mb-6 text-4xl lg:text-[48px] leading-[1.2] md:leading-[1] lg:leading-[0.8]"
             >
               <span className="mr-4 text-white">Join us and</span>
               <TypeAnimation
@@ -79,8 +79,6 @@ const MaskingText = () => {
                   "Unite",
                   2000,
                   "Thrive",
-                  2000,
-                  "Collaborate",
                   2000,
                   "Succeed",
                   2000,
